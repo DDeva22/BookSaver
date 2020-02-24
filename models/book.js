5902648ff8e47c859eb6a25bb66569a583030f0b
danman15726 <danman15726@gmail.com>
2020-02-23 22:33:28 -0600

    added some structure similar to other MERN apps. Created the book model according to the requirements.

---
+++ models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema ({
+    title: { type: String, require: true },
+    author: { type: String, required: true},
+    description: {type: String},
+    image: { type: String, required: true },
+    link: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+
+module.exports = Book;
