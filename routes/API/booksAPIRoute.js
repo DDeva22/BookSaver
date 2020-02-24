625087741920eb3f30cbac30290577401013fc52
danman15726 <danman15726@gmail.com>
2020-02-23 23:51:12 -0600

    Added a basic books API route that does dedicated function calls based off the url. SO inquiryController DEFINES the calls, and booksAPIRoute makes the calls DEPENDING on url.

--- routes/API/booksAPIRoute.js
+++ routes/API/booksAPIRoute.js
@@ -2,3 +2,18 @@ const router =  require("express").Router();
 const inquiryController = require("../../controllers/inquiryController.js");
 
 
+router.route("/")
+    .get(inquiryController.findAll)
+    .post(inquiryController.create);
+
+
+router.route("/:id")
+    .get(inquiryController.findById)
+    .put(inquiryController.update)
+    .delete(inquiryController.remove);
+
+
+
+
+
+module.exports = router;
