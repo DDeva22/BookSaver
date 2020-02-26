73566f9c8d3c6ab9227e6fe980423c09a671f255
Dan Devarajan <ddevarajan22@gmail.com>
2020-02-25 21:40:59 -0600

    Refactored the pages/AddBook.js by moving code directly to searchForm.js. Renamed variables as well to correctly call and refer to each other.

--- client/src/components/searchForm.js
+++ client/src/components/searchForm.js
@@ -1,39 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API.js";
 
-const Search = (prop) => (
-    <div className="row justify-content-center">
-        <div className="col-12 col-md-10 col-lg-8">
-            <form className="card card-sm">
-                <div className="card-body row no-gutters align-items-center">
-                    <div className="col-auto">
-                        <i className="fas fa-search h4 text-body"></i>
-                    </div>
-                    
-                    <div className="col">
-                        <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search Title or Author" value = {prop.query} onChange = {prop.setSearch} ></input>
-                    </div>
-                    
-                    <div className="col-auto">
-                        <button className="btn btn-lg btn-success" type="submit" onClick = {prop.submit}>Search</button>
-                    </div>
-                   
-                </div>
-            </form>
+
+const Search = (props) => {
+    const [search, setSearch] = useState("")
+
+    const submit = (event) => {
+        event.preventDefault();
+
+        API.search(search)
+        .then(res => (
             
-            
+            console.log(res.data.items[0])
+        ))
+        .catch(error => console.log(error));
+    };
+
+    
+
+
+
+    return(
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-8">
+                <form className="card card-sm">
+                    <div className="card-body row no-gutters align-items-center">
+                        <div className="col-auto">
+                            <i className="fas fa-search h4 text-body"></i>
+                        </div>
+                        
+                        <div className="col">
+                            <input 
+                            className="form-control form-control-lg form-control-borderless" 
+                            type="text" 
+                            placeholder="Search Title or Author" 
+                            value = {search} 
+                            onChange = {(e)=> setSearch(e.target.value)} ></input>
+                        </div>
+                        
+                        <div className="col-auto">
+                            <button className="btn btn-lg btn-success" type="submit" onClick = {submit}>Search</button>
+                        </div>
+                    
+                    </div>
+                </form>
+                
+                
+            </div>
         </div>
-    </div>
 
 
 
 
 
 
+    );
 
 
 
 
 
-);
+};
 
 export default Search;
--- client/src/pages/AddBook.js
+++ client/src/pages/AddBook.js
@@ -7,32 +7,10 @@ import Search from "../components/searchForm.js";
 
 
 const AddBook = () => {
-    const [search, setSearch] = useState({})
+   
 
 
-    const submit = (event) => {
-        event.preventDefault();
-
-        API.search(search)
-        .then(res => {(
-            setSearch(res))
-            console.log(res.data.items[0])
-        })
-        .catch(error => console.log(error));
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
     return (
@@ -40,18 +18,13 @@ const AddBook = () => {
             <Jumbotron
                 header = {"Add A Book!"}
                 sentence = {"Enter By Title or Author"}
+                
             />
             
             <Search
-            submit = {submit}
-            setSearch = {setSearch}
+                
+            
             />
-
-
-
-
-
-
 
 
 
