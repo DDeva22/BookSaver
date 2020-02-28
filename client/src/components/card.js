6f179974c45792d00cc287708bf8efc36a16ad86
danman15726 <danman15726@gmail.com>
2020-02-28 14:11:44 -0600

    Fixed image links not showing where appropriate. Also fixed cards showing from left to right, rather than top to bottom

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -3,14 +3,21 @@ import React from "react";
 
 const Card = (prop) => (
 
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card flex-row flex-wrap">
         
         <div className="card-body">
-            <h5 className="card-title">{prop.bookTitle}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{prop.subTitle}</h6>
-            <p className="card-text">{prop.synopsis}</p>
-            <a href= {prop.view} className="card-link">View</a>
-            <a href="#" className="card-link">Save</a>
+            <div className = "card-header border-0">
+                <img src = {prop.image}></img>
+            </div>
+            <div className = "card-block px-2">
+                <h5 className="card-title">{prop.bookTitle}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{prop.subTitle}</h6>
+                <p className="card-text">{prop.synopsis}</p>
+                <a href= {prop.view} className="card-link">View</a>
+                <a href={prop.save} className="card-link">Save</a>
+
+            </div>
+            
         </div>
     </div>
 
--- client/src/pages/AddBook.js
+++ client/src/pages/AddBook.js
@@ -5,24 +5,44 @@ import Card from "../components/card.js";
 import Navbar from "../components/navbar.js";
 import Search from "../components/searchForm.js";
 
-
 const AddBook = () => {
     const [search, setSearch] = useState("")
+    const [results, setResults] = useState([]);
 
-
+    useEffect(() => {
+        
+        console.log(results)
+    }, [results]);
 
 
     const setChange = (event) => setSearch(event.target.value)
 
 
     const handleFormSubmit = (event) => {
-        
+        event.preventDefault();
         API.search(search)
         .then(res => (
-            setSearch(res.data.message),
-            console.log(res))
+            
+            setResults(res.data.items))
         )
         .catch(error => console.log(error));
+
+    }
+
+
+    const validate = (input) => {
+        if(typeof input === 'undefined' || typeof input === undefined || typeof input === null) {
+            return "String of Undefined"
+        } else{
+            return input;
+
+        }
+        
+
+
+
+
+
     }
 
 
@@ -30,29 +50,36 @@ const AddBook = () => {
 
 
 
-
-
-
-
-
-
-
-    
-
-
     return (
         <div>
             <Jumbotron
                 header = {"Add A Book!"}
-                sentence = {"Enter By Title or Author"}
-                
+                sentence = {"Enter By Title or Author"}  
             />
-            
             <Search
-            setChange = {setChange}
-            submit = {handleFormSubmit}
-            
+                setChange = {setChange}
+                submit = {handleFormSubmit}
             />
+
+            <ul>
+                {results.map((result, i) => (
+                    <li key = {i}>
+                        <Card 
+                            
+                            bookTitle = {result.volumeInfo.title}
+                            subTitle = {result.volumeInfo.authors}
+                            synopsis = {result.volumeInfo.description}
+                            view = {result.volumeInfo.infoLink}
+                            image = {result.volumeInfo.imageLinks === undefined
+                                ? ""
+                                : `${result.volumeInfo.imageLinks.thumbnail}`
+                            }
+
+                        />
+                    </li>
+                ))}
+            </ul>
+            
 
 
 
